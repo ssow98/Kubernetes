9efdbb976808356546eb0c5cfde79d9f577550e0
ssow98 <sow.souleymane1998@gmail.com>
2024-02-08 19:48:19 +0100

    feat(k8s): add init minik8s cluster with argocd install

--- miniK8s/init_minik8s_cluster.sh
+++ miniK8s/init_minik8s_cluster.sh
@@ -10,9 +10,16 @@ install_argocd() {
     kubectl create namespace argocd
     helm repo add argo https://argoproj.github.io/argo-helm
     helm repo update
-    helm install argocd argo/argo-cd -n argocd
+    helm install argocd argo/argo-cd -n argocd -f https://github.com/ssow98/Kubernetes/blob/main/apps/init_k8s_cluster.sh
+}
+
+install_argocd_cli(){
+    curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+    rm argocd-linux-amd64
 }
 
 # Installation de Nginx Ingress Controller et ArgoCD
 install_nginx_ingress
 install_argocd
+install_argocd_cli
